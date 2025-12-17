4dd794b7a677b9cfe62e2802513d531b51b9e964
Gexnys <developergokhan@proton.me>
2025-12-18 01:16:08 +0300

    Add files via upload

--- src/module-info.java
+++ src/module-info.java
@@ -1,1 +1,10 @@
+module LustraShoes {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	requires javafx.graphics;
+	opens application to javafx.graphics, javafx.fxml,javafx.base;
+   
 
+}
